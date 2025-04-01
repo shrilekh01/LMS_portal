baf779dce5a275f2514f5a0288473fbc70b9b78d
shrilekh01 <shrilekhshrikhande06@gmail.com>
2025-04-01 10:35:36 +0530

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <ul>
+      <li className='bg-blue-400'>Shrilekh</li>
+      <li className='bg-orange-400'>Devanshu</li>
+      <li className='bg-green-400'>Mohit</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
